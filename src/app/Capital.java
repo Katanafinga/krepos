9ab1cf0151fad34483d53c09ef0db09d236860d1
Katanafinga <johndlenn9@gmail.com>
2022-06-02 12:59:42 -0400

    landi

--- src/app/Capital.java
+++ src/app/Capital.java
@@ -4,5 +4,6 @@ public class Capital {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Helloeffo World");
+		System.out.println("Landi World");
 	}
 }
